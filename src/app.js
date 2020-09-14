f3d7107f81ba203646e6be4ac80edee2ba3390db
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-09-14 20:35:08 +0200

    html done
    css
    
    modif css header section footer
    
    Validation accessibilité ok
    
    Create README.md
    
    CSS portfolio
    
    link tools
    
    Test table
    
    test tableau
    
    Add formulaire
    
    Add send button
    
    First mobile done. Start responsive
    
    Footer Responsive
    
    Header responsive
    
    Update README.md
    
    Start schema.org
    
    Add border nav
    
    Modif font-size
    
    footer ok
    
    Add js
    
    web_portfolio_v1 done
    
    Correction orthographe + target_blank
    
    Correction ortographe
    
    Add Etch a sketch - Portfolio
    
    Maj Access
    
    little refactor (HTML CSS): correction
    
    modif HTML JS: Corrrection
    
    Add Tictactoe
    
    Add icone
    Indication mobile et 1 ou 2 joueurs
    
    Add app météo js
    
    Correction animation portfolio
    
    V1.1 add go up btn
    
    Test add progress bar

---
+++ src/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let icone = document.querySelectorAll("i");
+
+  for (let i = 0; i < icone.length - 3; i++) {
+    icone[i].addEventListener("mouseover", () => {
+      icone[i].classList.toggle("colored");
+    });
+  }
+  for (let i = 0; i < icone.length - 3; i++) {
+    icone[i].addEventListener("click", () => {
+      icone[i].classList.toggle("colored");
+    });
+  }
+
+  // Progress bar
+
+  let barProgress = document.getElementById("myBar");
+  
+  window.onscroll = function () {
+    progressScroll();
+  };
+
+  let progressScroll = () => {
+    let winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    barProgress.style.width = scrolled + "%";
+  };
+});
