f876c8d05390b7da31d8cdc1ab6adafe9302e92e
Yang <nicolas.yang.1@gmail.com>
2020-10-03 09:40:58 +0200

    modif HTML JS: Corrrection

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let icone = document.querySelectorAll("i")
 
-    for (let i = 0; i < 27; i++) {
+    for (let i = 0; i < 26; i++) {
         icone[i].addEventListener('mouseover', () => {
             icone[i].classList.toggle('colored')
         })
     }
-    for (let i = 0; i < 27; i++) {
+    for (let i = 0; i < 26; i++) {
         icone[i].addEventListener('click', () => {
             icone[i].classList.toggle('colored')
         })
