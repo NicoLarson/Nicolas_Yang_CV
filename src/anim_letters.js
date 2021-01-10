9ccd51a448fba7b290da951308b3c0ae7682d6d5
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-01-10 20:13:18 +0100

    Almost done

--- src/anim_letters.js
+++ src/anim_letters.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.querySelector("#welcome ul");
-  let sentence = "Bonjour!";
+  let sentence = "Bienvenue!";
 
   for (let i = 0; i < sentence.length; i++) {
     el.innerHTML += `<li>${sentence[i]}</li>`;
--- src/dark_mode.js
+++ src/dark_mode.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 const btn = document.querySelector('header i');
 const headerSection = document.querySelector('header');
 const headerTitleOne = document.querySelector('header h1');
-const headerIcons = document.querySelector('header i');
+const headerIconsYingYang = document.querySelector('header i');
+const headerIconsBurger = document.querySelector('header nav ul i');
+
 
 const welcomeSection = document.querySelector(' main #welcome');
 const welcomeSectionTitleTwo = document.querySelector('#welcome h2');
@@ -13,12 +15,13 @@ const  skillsSection = document.querySelector('#skills')
 const  skillsSectionSection = document.querySelector('#skills section')
 
 const  toolsSection = document.querySelector('#tools')
-const  toolsSectionSection = document.querySelector('#tools section')
+const  toolsSectionSection = document.querySelector('#tools>section')
 
 btn.addEventListener('click',()=>{
     headerSection.classList.toggle('dark-mode')
     headerTitleOne.classList.toggle('dark-mode')
-    headerIcons.classList.toggle('dark-mode')
+    headerIconsYingYang.classList.toggle('dark-mode')
+    headerIconsBurger.classList.toggle('dark-mode')
 
     welcomeSection.classList.toggle('dark-mode')
     welcomeSectionTitleTwo.classList.toggle('dark-mode')
