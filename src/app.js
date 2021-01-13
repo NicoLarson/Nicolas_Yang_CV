e82803bbb2233154c7c6c8fd9bbabf9cdca0d668
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-01-13 15:26:12 +0100

    update transition darkmode

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
   // Hover Effect
   /* let icone = document.querySelectorAll("i");
 
@@ -13,20 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
       icone[i].classList.toggle("colored");
     });
   } */
-
+  
   // Progress bar
 
   let barProgress = document.getElementById("myBar");
-  
+  let barHide = document.querySelector("#barHide");
+  let prevScrollpos = window.pageYOffset;
+
   window.onscroll = function () {
     progressScroll();
+    hideOnScroll()
   };
 
   let progressScroll = () => {
     let winScroll =
       document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
     let scrolled = (winScroll / height) * 100;
     barProgress.style.width = scrolled + "%";
   };
+
+  let hideOnScroll = () => {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      barHide.style.top = "0";
+    } else {
+      barHide.style.top = "-12rem";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
 });
