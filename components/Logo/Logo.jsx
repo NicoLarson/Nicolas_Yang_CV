33c5edfe0a264cde1f092c99ceaba7a448bf47e3
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-05-26 14:40:33 -0300

    Update style

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
+import LogoPic from './logo.png'
+import styles from './Logo.module.css'
 
 export default function Logo() {
     return (
         <>
-        <p>LOGO</p>
+            <Image src={LogoPic} alt="logo"
+                width={60 + 'rem'}
+                height={50 + 'rem'}
+            />
         </>
     )
+
 }
